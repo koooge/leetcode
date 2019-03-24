1af9b23fd6bf81caa02d769877ffae1afee9bbda
koooge <koooooge@gmail.com>
2019-03-24 08:11:26 +0000

    2: wrong ans 2
    Signed-off-by: koooge <koooooge@gmail.com>

--- 2/2.js
+++ 2/2.js
@@ -6,30 +6,24 @@ class ListNode {
 }
 
 const addTwoNumbers = (l1, l2) => {
-  let num1 = num2 = 0;
+  let head = new ListNode(0, null);;
 
-  for (let digit = 1, node1 = l1, node2 = l2;
-    node1 !== null && node2 !== null;
-    digit *= 10, node1 = node1.next, node2 = node2.next) {
-    num1 += node1.val * digit;
-    num2 += node2.val * digit;
-  }
-  const sum = num1 + num2;
-  
-  let arr = [];
-  if (sum === 0) {
-    arr.push(new ListNode(0, null));
-  } else {
-    for (let num = sum; num !== 0; num = Math.floor(num / 10)) {
-      arr.push(new ListNode(num % 10, null));
-    }
+  let carry = 0;
+  for (let prev = head, node1 = l1, node2 = l2;
+    node1 !== null || node2 !== null;
+    node1 = node1 ? node1.next : null, node2 = node2 ? node2.next : null) {
+
+    const val1 = node1 ? node1.val : 0;
+    const val2 = node2 ? node2.val : 0;
+    const sum  =  val1 + val2 + carry;
+    carry = sum >= 10 ? 1 : 0;
+
+    const node = new ListNode(Math.floor(sum % 10), null);
+    prev.next = node;
+    prev = node; 
   }
 
-  for (let i = 0; i < arr.length - 1; ++i) {
-    arr[i].next = arr[i+1];
-  }
-
-  return arr[0];
+  return head.next;
 };
 
 // case1
@@ -46,3 +40,8 @@ console.log(addTwoNumbers(l1, l2)); // 0
 l1 = new ListNode(1, new ListNode(8, null));
 l2 = new ListNode(0, null);
 console.log(addTwoNumbers(l1, l2)); // 1 -> 8
+
+// case4
+l1 = new ListNode(5, null);
+l2 = new ListNode(5, null);
+console.log(addTwoNumbers(l1, l2)); // 0 -> 1
