acc32f680a93c670d8aa313c319b81fd0eb71713
koooge <koooooge@gmail.com>
2019-03-26 14:20:54 +0900

    20: wrong

---
+++ 20/20.js
@@ -0,0 +1,36 @@
+const OPENERS = ['(', '{', '['];
+
+const isExpected = (o, c) => {
+	if (!o) {
+		return OPENERS.includes(c);
+	} else if (o === '(') {
+		return OPENERS.includes(c) || (c === ')');
+	} else if (o === '{') {
+		return OPENERS.includes(c) || (c === '}');
+	} else if (o === '[') {
+		return OPENERS.includes(c) || (c === ']');
+	}
+
+	return false;
+};
+
+const isValid = (s) => {
+	let stack = [];
+	for (let i = 0; i < s.length; ++i) {
+		if (!isExpected(stack.slice(-1)[0], s[i])) return false;
+		if (OPENERS.includes(s[i])) {
+			stack.push(s[i]);
+		} else {
+			stack.pop();
+		}
+	}
+
+	return true
+};
+
+console.log(isValid('()')); // true
+console.log(isValid('()[]{}')); // true
+console.log(isValid('(]')); // false
+console.log(isValid('([)]')); // false
+console.log(isValid('{[]}')); // true
+console.log(isValid('[')); // false
