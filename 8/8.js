0c64f87a5eeb77a207c1d679cc23aa202f50bcec
koooge <koooooge@gmail.com>
2019-03-26 16:03:16 +0000

    8: wrong
    Signed-off-by: koooge <koooooge@gmail.com>

--- 8/8.js
+++ 8/8.js
@@ -2,7 +2,9 @@ const INT_MIN = -1 * Math.pow(2, 31);
 const INT_MAX = Math.pow(2, 31) - 1;
 
 const myAtoi = (str) => {
-  const firstNumber = Number(str.replace(/^\s+/, '').split(' ')[0]);
+  const s = str.replace(/^\s+/, '').split(' ')[0];
+  const firstNumber = Number(s.match(/^[0-9+-]+/));
+
   if (Number.isNaN(firstNumber)) {
     return 0;
   } else if (firstNumber < INT_MIN) {
@@ -20,3 +22,5 @@ console.log(myAtoi('4193 with words')); // 4193
 console.log(myAtoi('words and 987')); // 0
 console.log(myAtoi('-91283472332')); // -2147483648
 console.log(myAtoi('  -0012a42')); // -12
+console.log(myAtoi('+1')); // 1
+console.log(myAtoi('-5-')); // -1
