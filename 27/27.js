2101633b14f3b216c6badcdcc372f2d57387b1e3
koooge <koooooge@gmail.com>
2019-03-29 09:39:04 +0900

    27: javascript
    Signed-off-by: koooge <koooooge@gmail.com>

--- 27/27.js
+++ 27/27.js
@@ -1,6 +1,10 @@
 const removeElement = (nums, val) => {
-	for (let i = 0; i < nums.length; ++i) {
-		if (nums[i] === val) nums.splice(i, 1);
+	let len = nums.length;
+	for (let i = 0; i < len; ++i) {
+		if (nums[i] === val) {
+			nums.splice(i--, 1);
+			--len;
+		}
 	}
 	return nums.length;
 };
@@ -9,5 +13,5 @@ const a = [3, 2, 2, 3];
 removeElement(a, 3) // a = [2, 2]
 console.log(a); // 2
 const b = [0, 1, 2, 2, 3, 0, 4, 2];
-removeElement(b, 2); // b = [0, 1, 4, 0, 3
+removeElement(b, 2); // b = [0, 1, 4, 0, 3]
 console.log(b); // 5
