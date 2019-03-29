22c6963e5c35d83f54c70517a93bbfa4342151e1
koooge <koooooge@gmail.com>
2019-03-29 10:36:41 +0900

    15: time limit exceeded
    Signed-off-by: koooge <koooooge@gmail.com>

--- 15/15.js
+++ 15/15.js
@@ -9,7 +9,9 @@ const threeSum = (nums) => {
 	});
 
 	for (let i = 0; i < nums.length - 2; ++i) {
+		if (nums[i] === nums[i - 1]) continue;
 		for (let j = i + 1; j < nums.length - 1 ; ++j) {
+			if (j > i + 1 && nums[j] === nums[j - 1]) continue;
 			let rest = -1 * (nums[i] + nums[j]) || 0;
 			let index = nums.lastIndexOf(rest);
 			if (index !== -1 && index > j) {
@@ -25,3 +27,4 @@ const threeSum = (nums) => {
 
 console.log(threeSum([-1, 0, 1, 2, -1, -4])); // [[-1, 0, 1], [-1, -1, 2]]
 console.log(threeSum([0, 0, 0])); // [[0, 0, 0]]
+console.log(threeSum([-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6]));  // [[-4,-2,6],[-4,0,4],[-4,1,3],[-4,2,2],[-2,-2,4],[-2,0,2]]
