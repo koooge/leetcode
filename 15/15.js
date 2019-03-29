c98c948265adc3b3b2c693c268247b6332147065
koooge <koooooge@gmail.com>
2019-03-29 10:34:28 +0900

    15: wrong
    Signed-off-by: koooge <koooooge@gmail.com>

---
+++ 15/15.js
@@ -0,0 +1,27 @@
+const threeSum = (nums) => {
+	let solutions = [];
+	let sortedDedupedArray = [];
+
+	nums.sort((a, b) => {
+		if (a < b) return -1;
+		if (a > b) return 1;
+		return 0;
+	});
+
+	for (let i = 0; i < nums.length - 2; ++i) {
+		for (let j = i + 1; j < nums.length - 1 ; ++j) {
+			let rest = -1 * (nums[i] + nums[j]) || 0;
+			let index = nums.lastIndexOf(rest);
+			if (index !== -1 && index > j) {
+				let solution = [nums[i], nums[j], nums[index]];
+				if (JSON.stringify(solutions[solutions.length - 1]) !== JSON.stringify(solution)) solutions.push(solution);
+			}
+		}
+	}
+
+	return solutions;
+};
+
+
+console.log(threeSum([-1, 0, 1, 2, -1, -4])); // [[-1, 0, 1], [-1, -1, 2]]
+console.log(threeSum([0, 0, 0])); // [[0, 0, 0]]
