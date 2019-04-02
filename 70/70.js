0367d07438d6716e2ae2422fc72d04bb9b9d7d84
koooge <koooooge@gmail.com>
2019-04-02 14:54:46 +0900

    70: javascript :+1:
    Signed-off-by: koooge <koooooge@gmail.com>

--- 70/70.js
+++ 70/70.js
@@ -1,24 +1,20 @@
-const step = n => {
-	let stairs = 0;
-
-	if (n === 0 || n === 1) {
-		return 1;
-	} else if (n >= 2) {
-		stairs += step(n - 1);
-		stairs += step(n - 2);
-	}
-
-	return stairs;
-};
-
 const climbStairs = n => {
 	let stairs = 0;
 
-	stairs += step(n - 1);
-	stairs += step(n - 2);
+	if (n >= 4) {
+		stairs += climbStairs(Math.floor(n/2)) * climbStairs(n - Math.floor(n/2));
+		stairs += climbStairs(Math.floor(n/2) - 1) * climbStairs(n - Math.floor(n/2) - 1);
+	} else if (n === 3) {
+		return 3;
+	} else if (n === 2) {
+		return 2;
+	} else if (n === 1) {
+		return 1;
+	}
 
 	return stairs;
 };
 
 console.log(climbStairs(2)); // 2
 console.log(climbStairs(3)); // 3
+console.log(climbStairs(45)); // 1836311903
