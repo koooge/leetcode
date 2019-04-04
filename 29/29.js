0b5ef26228de64d748ddf93cf972c62f766e180b
koooge <koooooge@gmail.com>
2019-04-04 16:06:59 +0000

    29: javascript
    Signed-off-by: koooge <koooooge@gmail.com>

--- 29/29.js
+++ 29/29.js
@@ -1,11 +1,6 @@
 const divide = (dividend, divisor) => {
-  if (dividend < -Math.pow(2, 31)) dividend = -Math.pow(2, 31);
-  if (dividend > Math.pow(2, 31) - 1) dividend = Math.pow(2, 31) - 1;
-
   let negative = 1;
   if ((dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0)) negative = -1;
-
-  if (negative === 1 && dividend === -Math.pow(2, 31)) dividend = Math.pow(2, 31) - 1;
 
   let div = Math.abs(dividend);
   let d = Math.abs(divisor);
@@ -13,11 +8,23 @@ const divide = (dividend, divisor) => {
   let ans = 0;
 
   while (div >= d) {
-    div -= d;
-    ++ans;
+    let count = 1;
+    let base = d;
+    while (base <= (div >> 1)) {
+      base <<= 1;
+      count <<= 1;
+    }
+
+    div -= base;
+    ans += count;
   }
 
-  return negative === -1 ? -ans : ans;
+  ans = negative === -1 ? -ans : ans;
+
+  if (ans > Math.pow(2, 31) - 1) ans = Math.pow(2, 31) - 1;
+  if (ans < -Math.pow(2, 31)) ans = -Math.pow(2, 31);
+
+  return ans;
 };
 
 console.log(divide(10, 3)); // 3
@@ -25,3 +32,4 @@ console.log(divide(7, -3)); // -2
 console.log(divide(-2147483648, -1)); // 2147483647
 console.log(divide(-2147483648, 1)); // -2147483648
 console.log(divide(1, 1)); // 1
+console.log(divide(-2147483648, -2147483648)); // 1
