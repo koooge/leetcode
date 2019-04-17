b7514a9235bcb28acc65755c881062ecfe715075
koooge <koooooge@gmail.com>
2019-04-17 14:25:01 +0000

    40: wrong
    Signed-off-by: koooge <koooooge@gmail.com>

--- 40/40.js
+++ 40/40.js
@@ -22,3 +22,4 @@ const combinationSum2 = (candidates, target) => {
 
 console.log(combinationSum2([10,1,2,7,6,1,5], 8)); // [[1,7],[1,2,5],[2,6],[1,1,6]]
 console.log(combinationSum2([2,5,2,1,2], 5)); // [[1,2,2],[5]]
+console.log(combinationSum2([5,4,5,1,5,3,1,4,5,5,4], 10); // [[1,1,3,5],[1,1,4,4],[1,4,5],[5,5]]
