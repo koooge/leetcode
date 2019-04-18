5796afb6eb5c6af3b6efc742e900e1d9eacdb1d2
koooge <koooooge@gmail.com>
2019-04-18 11:41:39 +0900

    43: wrong
    Signed-off-by: koooge <koooooge@gmail.com>

---
+++ 43/43.js
@@ -0,0 +1,5 @@
+const multiply = (num1, num2) => String(Number(num1) * Number(num2));
+
+console.log(multiply('2', '3')); // '6'
+console.log(multiply('123', '456')); // '56088'
+console.log(multiply('123456689', '987654321')); // '121932631112635269'
