286e776bb8295b19fdef20034de30a5ff81d7a45
koooge <koooooge@gmail.com>
2019-05-02 06:21:30 +0000

    203: javascript :thinking:
    Signed-off-by: koooge <koooooge@gmail.com>

--- 203/203.js
+++ 203/203.js
@@ -7,11 +7,22 @@ class ListNode {
 
 const removeElements = (head, val) => {
   if (!head) return null;
-  if (head.val === val) return head.next;
-  if (!head.next) return head;
 
-  if (head.next.val === val) head.next = head.next.next;
-  removeElements(head.next, val);
+  if (head.val === val) {
+    let next = head.next;
+    while (next && next.val === val) next = next.next;
+    head = next;
+    if (head) removeElements(head, val);
+  } else if (head.next) {
+    if (head.next.val === val) {
+      let next = head.next
+      while (next && next.val === val) next = next.next;
+      head.next = next;
+      removeElements(head.next, val);
+    } else {
+      removeElements(head.next, val);
+    }
+  }
 
   return head;
 };
@@ -21,7 +32,12 @@ console.log(removeElements(a, 6)); // [1 -> 2 -> 3 -> 4 -> 5]
 
 const b = new ListNode(1, new ListNode(2));
 console.log(removeElements(b, 1)); // [2]
+
 const c = new ListNode(1);
 console.log(removeElements(c, 1)); // []
+
 const d = new ListNode(1, new ListNode(1));
 console.log(removeElements(d, 1)); // []
+
+const e = new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(1))));
+console.log(removeElements(e, 2)); // [1, 1]
