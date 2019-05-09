3bd8b43b5fbe214bc33f996e72d52ff2462bcfcf
koooge <koooooge@gmail.com>
2019-05-09 18:16:04 +0900

    344: javascript
    Signed-off-by: koooge <koooooge@gmail.com>

---
+++ 344/344.js
@@ -0,0 +1,4 @@
+const reverseString = s => s.reverse();
+
+console.log(reverseString(["h","e","l","l","o"])); // ["o","l","l","e","h"]
+console.log(reverseString(["H","a","n","n","a","h"])); // ["h","a","n","n","a","H"]
