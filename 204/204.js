7c298dd3fd08f41d2f778b4ac32f8606637a4da3
koooge <koooooge@gmail.com>
2019-05-13 19:16:22 +0900

    204: javascript
    Signed-off-by: koooge <koooooge@gmail.com>

--- 204/204.js
+++ 204/204.js
@@ -1,18 +1,23 @@
 const countPrimes = n => {
 	if (n <= 2) return 0;
-
-	let primes = [2];
-
-	for (let i = 3; i < n; i += 2) {
-		let j = 0;
-		for (; j < primes.length; ++j) {
-			if (i % primes[j] === 0) break;
-		}
-		if (j === primes.length) primes.push(i);
+		
+	let nonPrimeNums = new Array(n).fill(false);
+	let counter = 0;
+		
+	for (let i = 2; i < n; ++i) {
+		if (nonPrimeNums[i]) continue;
+		counter += 1;
+					
+		for (let j = 2; j < n; ++j) {
+			let num = i * j;
+			if (num >= n) break;
+			nonPrimeNums[num] = true;
+		} 
 	}
 
-	return primes.length;
+	return counter;
 };
 
 console.log(countPrimes(10)); // 4
 console.log(countPrimes(2)); // 0
+console.log(countPrimes(499979)); // 41537
