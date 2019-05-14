115f26769e364c39bcc67fd212c851f9cb9bde67
koooge <koooooge@gmail.com>
2019-05-14 09:43:23 +0900

    283: javascript
    Signed-off-by: koooge <koooooge@gmail.com>

--- 283/283.js
+++ 283/283.js
@@ -1,9 +1,9 @@
 const moveZeroes = nums => {
-	nums.sort((a, b) => a > b);
-
-	while(nums[0] === 0) {
-		nums.shift();
-		nums.push(0);
+	for (let i = nums.length; i >= 0; --i) {
+		if (nums[i] === 0) {
+			nums.splice(i, 1);
+			nums.push(0);
+		}
 	}
 };
 
@@ -14,3 +14,7 @@ console.log(a); // [1, 3, 12, 0, 0]
 const b = [0];
 moveZeroes(b);
 console.log(b); // [0]
+
+const c = [0, 0, 1];
+moveZeroes(c);
+console.log(c); // [1, 0, 0]
