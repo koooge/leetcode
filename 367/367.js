d736a09671fc619f07286f1ae02791514c8fcbb4
koooge <koooooge@gmail.com>
2019-05-24 11:26:46 +0900

    367: javascript
    Signed-off-by: koooge <koooooge@gmail.com>

---
+++ 367/367.js
@@ -0,0 +1,9 @@
+const isPerfectSquare = num => {
+	for (let i = 1; i * i <= num; ++i) {
+		if (i * i === num) return true;
+	}
+	return false;
+};
+
+console.log(isPerfectSquare(16)); // true
+console.log(isPerfectSquare(14)); // false
