0afb953eb8da82ed11d6839f681c75b729d3915e
koooge <koooooge@gmail.com>
2019-05-31 15:22:57 +0900

    350: javascript
    Signed-off-by: koooge <koooooge@gmail.com>

--- 350/350.js
+++ 350/350.js
@@ -1,15 +1,18 @@
 const intersect = (nums1, nums2) => {
+	nums1.sort((a, b) => a > b);
+	nums2.sort((a, b) => a > b);
+
+	let result = [];
+
 	for (let i = 0; i < nums1.length; ++i) {
-	 for (let j = 0; j < nums2.length; ++j) {
-			if (nums1[i] === nums2[j]) {
-				let num = 1;
-				for (let k = i + 1, l = j + 1; k < nums1.length && l < nums2.length && nums1[k] === nums2[l]; ++k, ++l) ++num;
-				console.log(i, i + num);
-				return nums1.slice(i, i + num);
-			}
-	 }
+		const index = nums2.indexOf(nums1[i]);
+		if (index !== -1) {
+			result.push(nums1[i]);
+			nums2.splice(index, 1);
+		}
 	}
-	return [];
+
+	return result;
 };
 
 console.log(intersect([1, 2, 2, 1], [2, 2])); // [2, 2]
@@ -17,3 +20,4 @@ console.log(intersect([4, 9, 5], [9, 4, 9, 8, 4])); // [4, 9]
 console.log(intersect([], [])); // []
 console.log(intersect([1], [1])); // [1]
 console.log(intersect([2, 1], [1, 2])); // [1, 2]
+console.log(intersect([1, 2, 2, 1], [2])); // [2]
