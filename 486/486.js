0c98e8d9430ca50af0c02ddc033c3d56731388df
koooge <koooooge@gmail.com>
2019-06-05 16:45:36 +0000

    486: javascript :thinking:
    Signed-off-by: koooge <koooooge@gmail.com>

--- 486/486.js
+++ 486/486.js
@@ -1,36 +1,18 @@
 const PredictTheWinner = nums => {
-  if (nums.legnth <= 2) return true;
-  return solve(0, 0, nums);
+  return solve(nums, 0, 0, 0);
 };
 
-const solve = (p1, p2, nums) => {
+const solve = (nums, p1, p2, turn) => {
   const len = nums.length;
+  if (len === 0) return p1 >= p2;
 
-  if (len === 1) {
-    return p1 + nums[0] > p2;
-  } else if (len === 2) {
-    p1 += Math.max(...nums);
-    p2 += Math.min(...nums);
-    return p1 > p2;
+  if (turn) {
+    return solve(nums.slice(1), p1, p2 + nums[0], 0) && solve(nums.slice(0, len - 1), p1, p2 + nums[len - 1], 0);
   }
-
-  // P1: left
-  if (nums[1] >= nums[len - 1]) {
-    if (solve(p1 + nums[0], p2 + nums[1], nums.slice(2))) return true;
-  } else {
-    if (solve(p1 + nums[0], p2 + nums[len - 1], nums.slice(1, len - 1))) return true;
-  }
-
-
-  // P1: right
-  if (nums[0] >= nums[len - 2]) {
-    if (solve(p1 + nums[len - 1], p2 + nums[0], nums.slice(1, len - 1))) return true;
-  } else {
-    if (solve(p1 + nums[len - 1], p2 + nums[len - 2], nums.slice(0, len - 2))) return true;
-  }
-
-  return false;
+  return solve(nums.slice(1), p1 + nums[0], p2, 1) || solve(nums.slice(0, len - 1), p1 + nums[len - 1], p2, 1);
 };
 
 console.log(PredictTheWinner([1, 5, 2])); // false
 console.log(PredictTheWinner([1, 5, 233, 7])); // true
+console.log(PredictTheWinner([0])); // true
+console.log(PredictTheWinner([2, 4, 55, 6, 8])); // false
