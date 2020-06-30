b8bfcf5e37d638111b37432a32d9c9fd07e973b6
koooge <koooooge@gmail.com>
2020-06-30 22:25:04 +0200

    1: python :+1:
    Signed-off-by: koooge <koooooge@gmail.com>

--- 1/1.py
+++ 1/1.py
@@ -2,10 +2,12 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict = {}
         for i in range(len(nums)):
-            for j in range(len(nums) - 1 - i):
-                if nums[i] + nums[i + 1 + j] == target:
-                    return [i, i + 1 + j]
+            if nums[i] in dict:
+                return [dict[nums[i]], i]
+            else:
+                dict[target - nums[i]] = i
 
 print(Solution().twoSum([2, 7, 11, 15], 9)) # [0, 1]
 print(Solution().twoSum([3, 2, 4], 6)) # [1, 2]
