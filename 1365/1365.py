19d706a880128cae9bf83948f8c712edf5f460a1
koooge <koooooge@gmail.com>
2021-01-22 16:58:17 +0100

    1365: python
    Signed-off-by: koooge <koooooge@gmail.com>

--- 1365/1365.py
+++ 1365/1365.py
@@ -2,14 +2,7 @@ from typing import List
 
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        result = [0] * len(nums)
-
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if i == j: continue
-                if nums[i] > nums[j]: result[i] += 1
-
-        return result
+        return [*map(sorted(nums).index, nums)]
 
 s = Solution()
 assert(s.smallerNumbersThanCurrent([8, 1, 2, 2, 3]) == [4, 0, 1, 1, 3])
