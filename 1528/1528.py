3e3ded28d241c507a54616d2bca4a4a7856c76d1
koooge <koooooge@gmail.com>
2021-01-26 22:13:31 +0100

    1528: python
    Signed-off-by: koooge <koooooge@gmail.com>

--- 1528/1528.py
+++ 1528/1528.py
@@ -2,10 +2,7 @@ from typing import List
 
 class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
-        res = [''] * len(s)
-        for i in range(len(s)):
-            res[indices[i]] = s[i]
-        return ''.join(res)
+        return ''.join(c for _, c in sorted(zip(indices, s)))
 
 s = Solution()
 assert(s.restoreString('codeleet', [4, 5, 6, 7, 0, 2, 1, 3]) == 'leetcode')
