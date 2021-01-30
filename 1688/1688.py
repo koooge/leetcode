a8f0f7e35f0ad0f4eb7fa220f801fe24b66ba224
koooge <koooooge@gmail.com>
2021-01-30 23:58:41 +0100

    1688: python
    Signed-off-by: koooge <koooooge@gmail.com>

---
+++ 1688/1688.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        if n == 1: return 0
+        elif n == 2: return 1
+
+        m = n // 2
+        if n % 2 != 0:
+            return m + self.numberOfMatches(m + 1)
+        else:
+            return m + self.numberOfMatches(m)
+
+s = Solution()
+assert(s.numberOfMatches(7) == 6)
+assert(s.numberOfMatches(14) == 13)
+assert(s.numberOfMatches(1) == 0)
