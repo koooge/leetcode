8d6e09a9ed1968635983f94d0be0fe1dc576332b
koooge <koooooge@gmail.com>
2021-02-26 12:43:22 +0100

    643: python
    Signed-off-by: koooge <koooooge@gmail.com>

--- 643/643.py
+++ 643/643.py
@@ -2,11 +2,11 @@ from typing import List
 
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        maxAve = sum(nums[:k]) / k
+        subSum = maxSum = sum(nums[:k])
         for i in range(1, len(nums) - (k - 1)):
-            subAve = sum(nums[i:i + k]) / k
-            if subAve > maxAve: maxAve = subAve
-        return maxAve
+            subSum = subSum - nums[i - 1] + nums[i + k - 1]
+            if subSum > maxSum: maxSum = subSum
+        return maxSum / k
 
 s = Solution()
 assert(s.findMaxAverage([1, 12, -5, -6, 50, 3], 4) == 12.75)
