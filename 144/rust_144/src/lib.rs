f7102231dd44af774baf08ba9e3ac8b0b2ce2bae
koooge <koooooge@gmail.com>
2022-02-11 20:00:33 +0100

    144: rust
    Signed-off-by: koooge <koooooge@gmail.com>

---
+++ 144/rust_144/src/lib.rs
@@ -0,0 +1,65 @@
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+  pub val: i32,
+  pub left: Option<Rc<RefCell<TreeNode>>>,
+  pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+  #[inline]
+  pub fn new(val: i32) -> Self {
+    TreeNode {
+      val,
+      left: None,
+      right: None
+    }
+  }
+}
+
+pub struct Solution {}
+
+use std::rc::Rc;
+use std::cell::RefCell;
+
+impl Solution {
+  pub fn preorder_traversal(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+    let mut res = Vec::new();
+    Solution::helper(root, &mut res);
+    res
+  }
+
+  fn helper(root: Option<Rc<RefCell<TreeNode>>>, vec: &mut Vec<i32>) {
+      if let Some(node) = root {
+          vec.push(node.borrow().val);
+          Solution::helper(node.borrow().left.clone(), vec);
+          Solution::helper(node.borrow().right.clone(), vec);
+      }
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn case1() {
+    let head = Some(Rc::new(RefCell::new(TreeNode::new(1))));
+    head.as_ref().unwrap().borrow_mut().right = Some(Rc::new(RefCell::new(TreeNode::new(2))));
+    head.as_ref().unwrap().borrow_mut().right.as_ref().unwrap().borrow_mut().left = Some(Rc::new(RefCell::new(TreeNode::new(3))));
+
+    assert_eq!(Solution::preorder_traversal(head), vec![1, 2, 3]);
+  }
+
+
+  #[test]
+  fn case2() {
+    assert_eq!(Solution::preorder_traversal(None), vec![]);
+  }
+
+  #[test]
+  fn case3() {
+    let head = Some(Rc::new(RefCell::new(TreeNode::new(1))));
+
+    assert_eq!(Solution::preorder_traversal(head), vec![1]);
+  }
+}
