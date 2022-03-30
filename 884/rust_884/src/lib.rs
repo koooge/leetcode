6c482cbe2967a4ae1583cb60df7e4c23f71bd7b1
koooge <koooooge@gmail.com>
2022-03-30 15:32:45 +0200

    884: rust
    Signed-off-by: koooge <koooooge@gmail.com>

---
+++ 884/rust_884/src/lib.rs
@@ -0,0 +1,35 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn uncommon_from_sentences(s1: String, s2: String) -> Vec<String> {
+        s1.split_whitespace()
+            .chain(s2.split_whitespace())
+            .fold(std::collections::HashMap::new(), |mut m, w| {
+                let c = m.entry(w).or_insert(0usize);
+                *c += 1;
+                m
+            })
+            .iter()
+            .fold(Vec::new(), |mut acc, (&s, &f)| {
+                if f == 1 {
+                    acc.push(s.to_owned())
+                }
+                acc
+            })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert!(Solution::uncommon_from_sentences(String::from("this apple is sweet"), String::from("this apple is sour")).iter().all(|item| {
+            vec![String::from("sweet"), String::from("sour")].contains(item)
+        }));
+        assert!(Solution::uncommon_from_sentences(String::from("apple apple"), String::from("banana")).iter().all(|item| {
+            vec![String::from("banana")].contains(item)
+        }));
+    }
+}
